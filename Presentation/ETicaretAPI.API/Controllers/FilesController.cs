d6affaab4467b00baeb17bda16cd320da6fc33cb
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2023-06-15 23:12:59 +0300

    Son güncelleme

--- Presentation/ETicaretAPI.API/Controllers/FilesController.cs
+++ Presentation/ETicaretAPI.API/Controllers/FilesController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ETicaretAPI.API.Controllers
 {
@@ -19,7 +18,7 @@ namespace ETicaretAPI.API.Controllers
         {
             return Ok(new
             {
-                Url = _configuration["BaseStorageUrl"]
+                Url =  _configuration["BaseStorageUrl"]
             });
         }
     }
