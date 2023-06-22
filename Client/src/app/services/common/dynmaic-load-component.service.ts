637ded0bcf291269ae948889e5570d76813b0c60
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2023-06-22 23:55:00 +0300

    Son güncelleme

--- Client/src/app/services/common/dynmaic-load-component.service.ts
+++ Client/src/app/services/common/dynmaic-load-component.service.ts
@@ -1,4 +1,4 @@
-import { ComponentFactoryResolver, Injectable } from '@angular/core';
+import { ComponentFactoryResolver, Injectable, ViewContainerRef } from '@angular/core';
 import { BaseComponent } from 'src/app/base/base.component';
 
 @Injectable({
@@ -6,9 +6,13 @@ import { BaseComponent } from 'src/app/base/base.component';
 })
 export class DynmaicLoadComponentService {
 
+  //ViewContainerRef = Dinamik olarak yüklenecek componenti içerisinde barındıran container'dır (Her dinamik yükleme sürecinde önceki view'leri clear etmemiz gerekmektedir.)
+  //ComponentFactory = Component'lerin instance'larını oluşturmak için kullanılan nesnedir.
+  //ComponentFactoryResolver = Belirli bir component için ComponentFactory'i rezolve eden sınıftır. İçerisindeki resolveComponentFactory fonskiyonu aracılığıyla ilgili componente dair bir ComponentFactory nesnesi olutşrup döner
+
   constructor(private componentFactoryResolver : ComponentFactoryResolver) { }
 
-  async loadComponent(component:Component){
+  async loadComponent(component:Component, viewContainerRef : ViewContainerRef){
     let _component : any = null;
 
     switch  (component){
@@ -16,6 +20,9 @@ export class DynmaicLoadComponentService {
         _component = await import("../../ui/components/baskets/baskets.component");
         break;
     }
+
+    viewContainerRef.clear();
+    return viewContainerRef.createComponent(this.componentFactoryResolver.resolveComponentFactory(_component))
   }
 }
 
